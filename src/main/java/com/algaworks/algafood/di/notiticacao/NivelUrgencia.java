829a7c6e9be6d701dffe5573a8c09f4efe10b3c5
Rafael Lucio <rafaellucio.developer@gmail.com>
2021-08-08 08:11:06 -0300

    update project to start

--- src/main/java/com/algaworks/algafood/di/notiticacao/NivelUrgencia.java
+++
@@ -1,5 +0,0 @@
-package com.algaworks.algafood.di.notiticacao;
-
-public enum NivelUrgencia {
-	URGENTE, SEM_URGENCIA
-}
--- src/main/java/com/algaworks/algafood/di/notiticacao/Notificador.java
+++
@@ -1,9 +0,0 @@
-package com.algaworks.algafood.di.notiticacao;
-
-import com.algaworks.algafood.di.model.Cliente;
-
-public interface Notificador {
-
-	void notificar(Cliente cliente, String mensage);
-
-}
