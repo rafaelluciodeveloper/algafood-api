adab6cafdbc0ad226d3eabd3658659568137759e
Rafael Lucio <rafaellucio.developer@gmail.com>
2021-11-02 17:40:54 -0300

    Implementing API Tests with REST Assured and Validating the HTTP Status Code

---
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaApi.java
@@ -0,0 +1,30 @@
+package com.algaworks.algafood;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CadastroCozinhaApi {
+
+    @LocalServerPort
+    private  int port;
+
+    @Test
+    public void deveRetornar200_QuandoConsultarCozinhas(){
+        RestAssured.given()
+                .basePath("/cozinhas")
+                .port(port)
+                .accept(ContentType.JSON)
+                .when()
+                    .get()
+                .then()
+                    .statusCode(HttpStatus.OK.value());
+    }
+}
--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -17,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CadastroCozinhaIntegrationTests {
+public class CadastroCozinhaIT {
 
     @Autowired
     private CozinhaService cozinhaService;
