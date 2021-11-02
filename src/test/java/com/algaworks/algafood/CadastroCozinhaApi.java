d5adc1049027b1e71f3ce4d05c9a8291751ed6c9
Rafael Lucio <rafaellucio.developer@gmail.com>
2021-11-02 17:53:59 -0300

    Validating the HTTP response body

--- src/test/java/com/algaworks/algafood/CadastroCozinhaApi.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaApi.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,5 +27,18 @@ public class CadastroCozinhaApi {
                     .get()
                 .then()
                     .statusCode(HttpStatus.OK.value());
+    }
+
+    @Test
+    public void deveConter4Cozinhas_QuandoConsultarCozinhas(){
+        RestAssured.given()
+                    .basePath("/cozinhas")
+                    .port(port)
+                    .accept(ContentType.JSON)
+                    .when()
+                        .get()
+                    .then()
+                        .body("" , Matchers.hasSize(4))
+                        .body("nome", Matchers.hasItems("Indiana","Tailandesa"));
     }
 }
